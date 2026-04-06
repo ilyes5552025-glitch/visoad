a43b8f93ac54bd5367cfda8376ca456414931c31
ilyes <ilyes555.2025@gmail.com>
2026-04-06 22:21:11 +0200

    Push latest changes

--- components/AdminPanel.jsx
+++ components/AdminPanel.jsx
@@ -270,7 +270,7 @@ function Login({ onLogin }) {
           </form>
 
           <p style={{ textAlign: "center", color: "var(--muted)", fontSize: 11, marginTop: 24, lineHeight: 1.6 }}>
-            Demo: ************
+            
           </p>
         </div>
       </div>
